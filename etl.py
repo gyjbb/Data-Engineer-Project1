cef91edcc5fdaee7e77f5409d7b109483213836a
Yingjie Gao <49602984+gyjbb@users.noreply.github.com>
2023-01-01 18:51:38 -0800

    Add files via upload

--- etl.py
+++ etl.py
@@ -4,7 +4,9 @@ import psycopg2
 import pandas as pd
 from sql_queries import *
 
-
+"""
+This function processes the song dataset and use the columns to create the songs and artists tables. 
+"""
 def process_song_file(cur, filepath):
     # open song file
     df = pd.read_json(filepath, lines=True)
@@ -17,7 +19,10 @@ def process_song_file(cur, filepath):
     artist_data = df[['artist_id', 'artist_name','artist_location','artist_longitude','artist_latitude']].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
-
+    
+"""
+This function processes the log dataset and create the time, users. The songplay dataset is created by log file data and the songs and artists tables together.
+"""
 def process_log_file(cur, filepath):
     # open log file
     df = pd.read_json(filepath, lines=True)
@@ -61,7 +66,9 @@ def process_log_file(cur, filepath):
         songplay_data = (pd.to_datetime(row.ts, unit='ms'), row.userId, row.level,songid,artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
-
+"""
+This function get all the data files and iterate over each one for processing.
+"""
 def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
@@ -80,7 +87,10 @@ def process_data(cur, conn, filepath, func):
         conn.commit()
         print('{}/{} files processed.'.format(i, num_files))
 
-
+        
+"""
+This is main function, can be executed to generate the final tables.
+"""
 def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
